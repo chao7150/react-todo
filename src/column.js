217d4f33522adf16e98c9cfaa100b692df8b3e2c
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2019-04-24 00:01:20 +0900

    first commit

---
+++ src/column.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const Column = (props) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const listItems = props.contents.map((content) => {
+    return <li>{content}</li>
+  })
+  const handleAddItem = () => {
+    if (inputValue === '') {
+      return
+    }
+    props.onAddItem(inputValue)
+    setInputValue('')
+  }
+
+  return (
+    <div>
+      <h2>{props.type}</h2>
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+      <button onClick={handleAddItem}>add</button>
+      <ul>{listItems}</ul>
+      {props.type === 'customs' ? <button onClick={props.onAddAll}>add all</button> : null}
+    </div>
+  )
+}
+
+export default Column
